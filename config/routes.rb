3f42dd3f0b42a8c7dfb9f1c28131554204609669
TakumiFujita <feelings.are.possibilities@gmail.com>
2019-01-26 15:22:05 +0900

    Merge remote-tracking branch 'shunke434343/master' into view_items_show
    # Conflicts:
    #	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
+  get 'items/show', to: 'items#show'
+=======
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "users/index", to: "users#index"
+>>>>>>> shunke434343/master
 end
