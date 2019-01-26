0b8138549cd9fed9ef1c65cf162f85b590e386f6
misako231 <melon_pal.231@icloud.com>
2019-01-26 16:26:02 +0900

    edit basic_auth
    本番環境のみ動作するように編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception #セキュリティ対策
 
   private
 
-# Basic認証
-  def basic_auth
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth # Basic認証
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
