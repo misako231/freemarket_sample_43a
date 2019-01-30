3ba30f56d4d8fba3c68c6ec4f3d65970d2fe4c00
shunke434343 <shunka4@gmail.com>
2019-01-30 13:56:36 +0900

    set route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "mercari/index", to: "mercari#index"
   get "products/new", to: "products#new"
+
+
+
+
+
+
+
+  # shimizu
+  get "users/honnin", to: "users#honnin"
 end
