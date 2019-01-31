0c8228e17beb956702705aabf2df73a749683345
misako231 <melon_pal.231@icloud.com>
2019-01-31 13:04:43 +0900

    edit routes name

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   get "mercari/index", to: "mercari#index"
   get "users/index", to: "users#index"
   get "users/signup", to: "users#new"
-  get "users/signup/registration", to: "users#new2"
+  get "users/signup/registration", to: "users#new_registration"
   get "users/signup/street_address", to: "users#new3"
-  get "users/signup/done", to: "users#new5"
-  get "users/login", to: "users#new10"
+  get "users/signup/done", to: "users#new_street_address"
+  get "users/login", to: "users#new_done"
   get "products/new", to: "products#new"
   get "users/logout", to: "users#logout"
 end
