b4f36d54a9cf92e10c6ee45dbd0d5cb913583c7d
misako231 <melon_pal.231@icloud.com>
2019-01-31 12:26:50 +0900

    edit users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,21 @@ class UsersController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def new2
+  end
+
+  def new3
+  end
+
+  def new5
+  end
+
+  def new10
+  end
+
   def logout
   end
 end
