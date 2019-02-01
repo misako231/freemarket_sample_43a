8deb20cab504d66476e8fc41cb63500184dd4bc3
shunke434343 <shunka4@gmail.com>
2019-02-01 14:19:57 +0900

    Add columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_31_123502) do
+ActiveRecord::Schema.define(version: 2019_02_01_034923) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(version: 2019_01_31_123502) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "postal_code", null: false
+    t.integer "prefecture", null: false
+    t.string "city", null: false
+    t.string "block", null: false
+    t.string "building"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["phone"], name: "index_users_on_phone", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
