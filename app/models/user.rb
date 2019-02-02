2e57155fe8a7463f54b9bfafa5d7d74122588246
shunke434343 <shunka4@gmail.com>
2019-02-02 12:28:52 +0900

    バリデーションを最低限に

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname,        presence: true, length: { maximum: 20 }
+  validates :password,        presence: true, length: { in: 6..128 }
+  validates :last_name,       presence: true, length: { maximum: 35 }
+  validates :first_name,      presence: true, length: { maximum: 35 }
+  validates :last_name_kana,  presence: true, length: { maximum: 35 },
+                             format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
+  validates :first_name_kana, presence: true, length: { maximum: 35 },
+                             format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
 end
