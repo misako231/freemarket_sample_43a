f1e4d09f65a6c88b33afe62f613aa4476e998b89
shunke434343 <shunka4@gmail.com>
2019-02-02 14:53:33 +0900

    Add years & months method in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,21 @@ class User < ApplicationRecord
                              format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
   validates :first_name_kana, presence: true, length: { maximum: 35 },
                              format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
+
+  def self.years
+   years = []
+   for year in 1900..2019 do
+     years << year
+   end
+   return years
+  end
+
+  def self.months
+   months = []
+   for month in 1..12 do
+     months << month
+   end
+   return months
+  end
+
 end
