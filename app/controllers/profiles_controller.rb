b47dee17e1fce6848fddfd8ea9795db71a25342e
shunke434343 <shunka4@gmail.com>
2019-02-03 17:29:26 +0900

    Setting routes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,22 @@
 class ProfilesController < ApplicationController
+  def index
+  end
+
   def new
+  end
+
+  def show
+  end
+
+  def indentification
+  end
+
+  def card
+  end
+
+  def logout
+  end
+
+  def done
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,24 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
    registrations: 'users/registrations'
   }
-  root to: 'mercari#index'
   get '/users', to: redirect("/users/sign_up")
-  resources :mypage, only: [:index, :new]
-  resources :profiles, only: [:index, :new]
-  get 'items/show', to: 'items#show'
-  get 'items/buy', to: 'items#buy'
-  get "users/index", to: "users#index"
-  get "mypage/signup", to: "mypage#new"
-  get "mypage/login", to: "mypage#login"
-  get "items/new", to: "items#new"
-  get "mypage/identification", to: "mypage#identification"
-  get "mypage/logout", to: "mypage#logout"
-  get "mypage/profile", to: "mypage#profile"
-  get "mypage/card", to: "mypage#card"
+  root to: 'mercari#index'
+  resources :mypage, only: [:new]
+  resources :items do
+    member do
+      get :buy
+    end
+  end
+  resources :users, only: [] do
+    resources :profiles, only: [:index, :new, :create, :show, :update] do
+      member do
+        get :identification
+        get :card
+      end
+      collection do
+        get :logout
+        get :done
+      end
+    end
+  end
 end
