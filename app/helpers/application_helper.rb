3c715326c9c3eb4bdabea5bbcbe7fd7eb3936323
shunke434343 <shunka4@gmail.com>
2019-02-06 12:49:22 +0900

    shift

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def get_child_from_root(root)
+    Category.children_of(root).all
+  end
+
+  def get_deeper_from_child(child)
+    Category.where("ancestry LIKE ?", "%/#{child.id}").all
+  end
 end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,10 +1,2 @@
 module CategoriesHelper
-
-  def get_child_from_root(root)
-    Category.children_of(root).all
-  end
-
-  def get_deeper_from_child(child)
-    Category.where("ancestry LIKE ?", "%/#{child.id}").all
-  end
 end
