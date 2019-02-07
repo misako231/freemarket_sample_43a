a72d7f682efa85b1017d22002e30002e69dec94b
misako231 <melon_pal.231@icloud.com>
2019-02-07 14:03:30 +0900

    remove migration file

--- db/migrate/20190206044302_remove_ancestry_depth_from_categories.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAncestryDepthFromCategories < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :categories, :ancestry_depth, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_044302) do
+ActiveRecord::Schema.define(version: 2019_02_04_073717) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
