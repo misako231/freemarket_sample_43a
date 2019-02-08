971fb8253774c96feafd8a5a00b1b896cc38919a
misako231 <melon_pal.231@icloud.com>
2019-02-08 17:55:34 +0900

    edit items_controller show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,7 @@
 class ItemsController < ApplicationController
   include GetCategories
   before_action :get_root
-
-  def show
-  end
+  before_action :set_item, only: [:show]
 
   def new
     @item = Item.new
@@ -19,10 +17,25 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @grandchild_category = Category.find(@item.category_id)
+    @child_category = set_ancestors(@grandchild_category).last
+    @parent_category = set_ancestors(@grandchild_category).first
+    @users_item = Item.where(user_id: @item.user_id).all
+  end
+
+
   private
 
   def item_params
     params.require(:item).permit(:name, :comment, :category_id, :brand_id, :shipping_fee, :prefecture_id, :days_to_ship, :price, :condition, :closed, item_photos_attributes: [:image]).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.includes([:user, :item_photos, :category]).find(params[:id])
+  end
+
+  def set_ancestors(category)
+    Category.ancestors_of(category)
+  end
 end
