6ebf48aff4e3bc11f46d6625a96b98cfc4c6aa5b
TakumiFujita <feelings.are.possibilities@gmail.com>
2019-02-08 11:54:38 +0900

    test_items_exhibit5

--- spec/models/item_photo_spec.rb
+++ spec/models/item_photo_spec.rb
@@ -5,7 +5,7 @@ describe ItemPhotoesController do
   describe '#create' do
     context "with valid attributes" do
       it "has a valid factory" do
-        image = build(:image_photo)
+        image = build(:item_photo)
         expect(image).to be_valid
       end
     end
