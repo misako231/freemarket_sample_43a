eb1a00e9c813eaf9a74d5b1c2cf443601c31dac4
TakumiFujita <feelings.are.possibilities@gmail.com>
2019-02-11 20:14:05 +0900

    sns_certification8

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,32 +1,4 @@
-# frozen_string_literal: true
-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
 
   def facebook
     callback_for(:facebook)
@@ -37,29 +9,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   private
-
   def callback_for(provider)
     provider = provider.to_s
     @user = User.find_oauth(request.env["omniauth.auth"])
-    # if @user.persisted?
-    #   sign_in_and_redirect @user, event: :authentication #after_sign_in_path_forと同じパス
-    #   set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-    # else
-      # session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
-      # @facebook_info_params = []
-    if provider == "facebook"
-      facebook_info = request.env["omniauth.auth"]
-      facebook_name = facebook_info[:info][:name]
-      facebook_email = facebook_info[:info][:email]
-      binding.pry
-      redirect_to new_user_registration_path(name: facebook_name, email: facebook_email)
-    elsif provider == "google"
-      google_info = request.env["omniauth.auth"]
-      google_name = google_info[:info][:name]
-      google_email = google_info[:info][:email]
-      redirect_to new_user_registration_path(name: google_name, email: google_email)
-    end
-    # end
+      omniauth_info = request.env["omniauth.auth"]
+      omniauth_name = omniauth_info[:info][:name]
+      omniauth_email = omniauth_info[:info][:email]
+      redirect_to new_user_registration_path(name: omniauth_name, email: omniauth_email)
   end
 
   def failure
