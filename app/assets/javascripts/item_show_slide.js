dd5323a7d484b64b57261eb5ee67df5b1725413f
misako231 <melon_pal.231@icloud.com>
2019-02-13 15:50:01 +0900

    edit javascript

--- app/assets/javascripts/item_show_slide.js
+++ app/assets/javascripts/item_show_slide.js
@@ -1,15 +1,16 @@
 $(document).on('turbolinks:load', function() {
   $(function (){
-    var image_count = $('.parent-item-container__item--main-content__item-photo__sub-containts').length
+      var image_count = $('.parent-item-container__item--main-content__item-photo__sub-containts').length
 
-    $('.parent-item-container__item--main-content__item-photo__main ul').slick({
-    autoplay: false,
-    arrows: false,
-    speed: 500,
-    slidesToScroll: 2,
-    waitForAnimate: false,
-    asNavFor: '.parent-item-container__item--main-content__item-photo__sub ul'
-    });
+      $('.parent-item-container__item--main-content__item-photo__main ul').slick({
+      autoplay: false,
+      arrows: false,
+      speed: 500,
+      slidesToScroll: 2,
+      waitForAnimate: false,
+      asNavFor: '.parent-item-container__item--main-content__item-photo__sub ul'
+      });
+
     $('.parent-item-container__item--main-content__item-photo__sub ul').slick({
       slidesToShow: image_count,
       focusOnSelect: true,
@@ -17,8 +18,8 @@ $(document).on('turbolinks:load', function() {
     });
 
     $('.parent-item-container__item--main-content__item-photo__sub ul li').on('mouseover', function(e) {
-      var $currentTarget = $(e.currentTarget),
-          index = $currentTarget.closest('.slick-slide').data('slick-index'),
+      var $currentTarget = $(e.currentTarget);
+          index = $currentTarget.closest('.slick-slide').data('slick-index');
           slickObj = $('.parent-item-container__item--main-content__item-photo__main ul').slick('getSlick');
       slickObj.slickGoTo(index, false);
     });
