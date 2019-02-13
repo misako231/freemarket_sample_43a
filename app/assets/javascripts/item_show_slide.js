18e00fe86b78758a30460561d73b0c4724c013d6
misako231 <melon_pal.231@icloud.com>
2019-02-13 15:01:22 +0900

    create js

---
+++ app/assets/javascripts/item_show_slide.js
@@ -0,0 +1,32 @@
+$(document).on('turbolinks:load', function() {
+  $(function (){
+    $('.parent-item-container__item--main-content__item-photo__main ul').slick({
+    autoplay: false,
+    arrows: false,
+    speed: 500,
+    slidesToScroll: 2,
+    waitForAnimate: false,
+    asNavFor: '.parent-item-container__item--main-content__item-photo__sub ul'
+    });
+    $('.parent-item-container__item--main-content__item-photo__sub ul').slick({
+      slidesToShow: 4,
+      focusOnSelect: true,
+      asNavFor: '.parent-item-container__item--main-content__item-photo__main ul'
+    });
+
+    $('.parent-item-container__item--main-content__item-photo__sub ul li').on('mouseover', function(e) {
+      var $currentTarget = $(e.currentTarget),
+          index = $currentTarget.closest('.slick-slide').data('slick-index'),
+          slickObj = $('.parent-item-container__item--main-content__item-photo__main ul').slick('getSlick');
+      slickObj.slickGoTo(index, false);
+    });
+  });
+
+  $('.parent-item-container__item--main-content__item-photo__sub-containts, >image').hover(
+    function() {
+      $(this).addClass('active');
+    },
+    function() {
+      $(this).removeClass('active');
+  });
+});
