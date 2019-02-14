1f3a07b454e25d7cfc63f4ce8b7424159d84156f
misako231 <melon_pal.231@icloud.com>
2019-02-14 14:01:02 +0900

    edit get_points

--- app/controllers/concerns/get_points.rb
+++ app/controllers/concerns/get_points.rb
@@ -2,7 +2,9 @@ module GetPoints
   private
 
   def total_point
-    points = PointRecord.where(user_id: current_user.id).all
-    @points = points.sum { |p| p[:point] }
+    if user_signed_in?
+      points = PointRecord.where(user_id: current_user.id).all
+      @points = points.sum { |p| p[:point] }
+    end
   end
 end
