1393dc2fb11b925588f74eb4732a446d7f0a72cc
fukumotomasashi <cvb25362@gmail.com>
2019-02-15 12:09:35 +0900

    jquery image upload

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,7 +1,9 @@
 $(document).on('turbolinks:load', function() {
   $(function () {
-  $('.btn__default').click(function(){
-    $('.modal').fadeIN();
-    $(this).prop("disabled", false);
+    $('.btn__default').click(function(){
+      $("body").append('<div id="delete-overlay"></div>');
+      $("#delete-overlay").fadeIn("normal");
+      $('.modal').fadeIn();
+    });
   });
 });
