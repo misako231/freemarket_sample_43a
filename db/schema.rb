ec51fcb3617850a865420fc1a659ae981999eb7d
misako231 <melon_pal.231@icloud.com>
2019-02-15 21:20:16 +0900

    create order_statuses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_065926) do
+ActiveRecord::Schema.define(version: 2019_02_13_111317) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -62,6 +62,18 @@ ActiveRecord::Schema.define(version: 2019_02_13_065926) do
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_items_on_name"
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "order_statuses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "purchaser_id"
+    t.bigint "seller_id"
+    t.integer "status", null: false
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_statuses_on_item_id"
+    t.index ["purchaser_id"], name: "index_order_statuses_on_purchaser_id"
+    t.index ["seller_id"], name: "index_order_statuses_on_seller_id"
   end
 
   create_table "point_records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -121,6 +133,9 @@ ActiveRecord::Schema.define(version: 2019_02_13_065926) do
   add_foreign_key "favorite_items", "users"
   add_foreign_key "item_photos", "items"
   add_foreign_key "items", "users"
+  add_foreign_key "order_statuses", "items"
+  add_foreign_key "order_statuses", "users", column: "purchaser_id"
+  add_foreign_key "order_statuses", "users", column: "seller_id"
   add_foreign_key "point_records", "users"
   add_foreign_key "profiles", "users"
 end
