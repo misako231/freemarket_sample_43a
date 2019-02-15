232c6173cdb3cf369f0eda357b0b1edd584dc14d
TakumiFujita <feelings.are.possibilities@gmail.com>
2019-02-15 20:10:22 +0900

    comments_function5

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,14 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    if current_user.id == item.user_id
+      comment.destroy
+      redirect_to "/items/#{@comment.item.id}"
+    end
+  end
+
   private
   def comment_params
     params.permit(:content).merge(user_id: current_user.id, item_id: params[:item_id])
