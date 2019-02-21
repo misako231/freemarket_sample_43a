49dd23034c2a80fec42c7494f5a7f5117c85c142
TakumiFujita <feelings.are.possibilities@gmail.com>
2019-02-21 15:33:31 +0900

    comment_function3

--- app/assets/javascripts/comment_delete.js
+++ app/assets/javascripts/comment_delete.js
@@ -1,10 +1,10 @@
 $(document).on('turbolinks:load', function() {
-  $(document).on("click", ".parent-comment-container__message-container-right__body-icons__trash-can", function(){
+  $(".parent-comment-container-inner").on("click", ".parent-comment-container__message-container-right__body-icons__trash-can", function(){
       $(".parent").append('<div id="delete-overlay"></div>');
-      $(".dialog-box").fadeIn("normal");
+      $(".dialog-box_comment").fadeIn("normal");
       $("#delete-overlay").fadeIn("normal");
       $(".delete-dialog__buttons__cancel,#delete-overlay").unbind().click(function(){
-        $(".dialog-box,#delete-overlay").hide();
+        $(".dialog-box_comment,,#delete-overlay").hide();
       });
     }
   );
